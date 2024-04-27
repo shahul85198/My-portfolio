b186708daf60377c513012b4b63775c9ca938e36
shahul85198 <124188392+shahul85198@users.noreply.github.com>
2024-04-27 15:35:34 +0000

    icons added

--- my-portfolio/src/app/page.tsx
+++ my-portfolio/src/app/page.tsx
@@ -1,18 +1,14 @@
-
 import Header from "./header/page";
 import Homeprofile from "./portfoliohomepage/homeprofile/homeprofile";
 
 export default function Home() {
-  
   return (
-    <div className="bg-slate-800 h-screen p-12 ">
-       <Header />
-    <div className="flex justify-center p-12 bg-slate-400 h-96">
-       
-     <Homeprofile /> 
-    
-    </div>
-     
+    <div className="bg-slate-800 h-screen p-12" style={{backgroundImage: "url('https://img.freepik.com/free-vector/paper-style-dynamic-lines-background_23-2149008629.jpg')", backgroundSize: "cover", backgroundPosition: "center"}}>
+      <Header />
+      <div className="flex justify-center b-2 border-red-500 pt-16">
+        <Homeprofile />
+      </div>
     </div>
   );
 }
+
--- my-portfolio/src/app/portfoliohomepage/homeprofile/homeprofile.tsx
+++ my-portfolio/src/app/portfoliohomepage/homeprofile/homeprofile.tsx
@@ -4,14 +4,14 @@ function Homeprofile() {
   return (
     <div className="">
       <div className="flex">
-        <div className="grid text-xl space-y-8">
+        <div className=" text-xl space-y-8 text-white">
        <h1>Hi There</h1>
        <h1>I'm Shahul Hameed</h1>
        <h3>A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks.</h3>
        <button className="bg-slate-900 w-48 p-4 text-white ">Contact Me...</button>
       </div>
       <div className="">
-        <img src="https://i.stack.imgur.com/TSsVA.png?s=256&g=1" className="w-96" />
+        <img src="https://i.pinimg.com/736x/60/0d/c8/600dc8207b7d86e7e3975b862ce858c2.jpg" className="" />
         </div>
         </div>
     </div>
